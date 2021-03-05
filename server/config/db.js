076faef5ec439c8fd09088d59f37ef5ea8e82c58
nidhitrivedi09 <tnidhi518@gmail.com>
2021-03-05 13:21:21 +0530

    changed uri

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,5 @@ Nidhi Trivedi , 301172350 , 01/03/2021 */
 module.exports = {
   //local MongoDB deployment ->
  "URI": "mongodb://nidhitrivedi:nidhi1999@portfolio-cluster-shard-00-00.dwiww.mongodb.net:27017,portfolio-cluster-shard-00-01.dwiww.mongodb.net:27017,portfolio-cluster-shard-00-02.dwiww.mongodb.net:27017/mydb?ssl=true&replicaSet=atlas-n1rtqt-shard-0&authSource=admin&retryWrites=true&w=majority"
-
   //"URI": "mongodb://localhost/books229"
 };
